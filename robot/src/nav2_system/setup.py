15dd932d8139d727edc3ea8bdf37494d4fc08526
Kitsune Yae <yaekitsune2207@gmail.com>
2025-03-30 13:45:24 +0200

    Carga bien el mapa
    Por fin Navegacion?

--- robot/src/nav2_system/setup.py
+++ robot/src/nav2_system/setup.py
@@ -26,6 +26,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'initial_pose_pub = nav2_system.initial_pose_pub:main' #añadir
         ],
     },
 )
