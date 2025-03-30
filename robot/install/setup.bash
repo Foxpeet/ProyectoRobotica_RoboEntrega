10310c6e008e56e332f5b9a274a4be9fabfcfe1e
Kitsune Yae <yaekitsune2207@gmail.com>
2025-03-30 12:52:41 +0200

    Testing

--- robot/install/setup.bash
+++ robot/install/setup.bash
@@ -22,9 +22,6 @@ _colcon_prefix_chain_bash_source_script() {
 COLCON_CURRENT_PREFIX="/opt/ros/galactic"
 _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
-COLCON_CURRENT_PREFIX="/home/haoxu/turtlebot3_ws/src/my_slam/install"
-_colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
-# setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
 COLCON_CURRENT_PREFIX="/home/haoxu/turtlebot3_ws/install"
 _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
 
--- robot/install/setup.ps1
+++ robot/install/setup.ps1
@@ -23,7 +23,6 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/galactic\local_setup.ps1"
-_colcon_prefix_chain_powershell_source_script "/home/haoxu/turtlebot3_ws/src/my_slam/install\local_setup.ps1"
 _colcon_prefix_chain_powershell_source_script "/home/haoxu/turtlebot3_ws/install\local_setup.ps1"
 
 # source this prefix
