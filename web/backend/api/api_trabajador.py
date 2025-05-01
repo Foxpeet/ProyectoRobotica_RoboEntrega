0a6d2d8b3a197b4fa89823ac9041abc398f00094
Kitsune Yae <yaekitsune2207@gmail.com>
2025-05-01 17:47:17 +0200

    QT-H6-20 Interaccion frontend-backend
    Ya se puede conectar a la bbdd.
    Ahora toca modificar cada api, a lo que queremos

---
+++ web/backend/api/api_trabajador.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, request, jsonify
+from extensions import db
+from models.trabajador import Trabajador
+
+trabajador_api = Blueprint('api_trabajador', __name__)
+
+@trabajador_api.route('/trabajadores', methods=['GET'])
+def get_trabajadores():
+    trabajadores = Trabajador.query.all()
+    return jsonify([
+        {
+            'dni_trabajador': t.dni_trabajador,
+            'nombre_trabajador': t.nombre_trabajador,
+            'apellido_trabajador': t.apellido_trabajador,
+            'correo': t.correo,
+            'presente': t.presente,
+            'rol_admin': t.rol_admin,
+            'mesa_id_mesa': t.mesa_id_mesa
+        }
+        for t in trabajadores
+    ])
+
+@trabajador_api.route('/trabajadores', methods=['POST'])
+def create_trabajador():
+    data = request.get_json()
+
+    nuevo = Trabajador(
+        dni_trabajador=data['dni_trabajador'],
+        nombre_trabajador=data['nombre_trabajador'],
+        apellido_trabajador=data['apellido_trabajador'],
+        correo=data['correo'],
+        contraseña_hash=data['contraseña_hash'],
+        presente=data.get('presente', False),
+        rol_admin=data.get('rol_admin', False),
+        mesa_id_mesa=data['mesa_id_mesa']
+    )
+    db.session.add(nuevo)
+    db.session.commit()
+
+    return jsonify({'message': 'Trabajador añadido correctamente'}), 201
---
+++ web/backend/app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from extensions import db
+from flask_migrate import Migrate
+from flask_cors import CORS
+import os
+from api.api_robot import robot_api
+from api.api_trabajador import trabajador_api
+
+app = Flask(__name__)
+
+# Ruta absoluta dinámica al archivo SQLite
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, 'instance', 'robo_entrega.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicializa la db con la app
+db.init_app(app)
+migrate = Migrate(app, db)
+
+# Registrar el Blueprint
+app.register_blueprint(robot_api, url_prefix='/api')
+app.register_blueprint(trabajador_api, url_prefix='/api')
+
+CORS(app)
+
+# Importa los modelos después de inicializar db
+with app.app_context():
+    from models.robot import Robot
+    from models.mesa import Mesa
+    from models.trabajador import Trabajador
+    from models.entregas import Entrega  
+    from models.paquetes import Paquete  
+    from models.documentos import Documento
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
