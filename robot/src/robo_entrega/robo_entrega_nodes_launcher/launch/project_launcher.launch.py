35211ec331b8e38dca3d62d2dfa930a2c336e3ca
foxpeet <diegobal007@gmail.com>
2025-06-02 16:06:32 +0200

    QT-H9-26 Actualizado nodes_launcher sin el nav_to_pose

--- robot/src/robo_entrega/robo_entrega_nodes_launcher/launch/project_launcher.launch.py
+++ robot/src/robo_entrega/robo_entrega_nodes_launcher/launch/project_launcher.launch.py
@@ -17,12 +17,6 @@ def generate_launch_description():
     map_file = os.path.join(get_package_share_directory('robo_entrega_nav2_system'), 'config', 'my_map.yaml')
     rviz_config_dir = os.path.join(get_package_share_directory('robo_entrega_nav2_system'), 'config', 'map.rviz')
 
-
-    #nodo nav_to_pose
-    launch_nav_to_pose = ExecuteProcess(
-        cmd=['ros2', 'launch', 'robo_entrega_nav_to_pose', 'nav_to_pose_service.launch.py']
-    )
-
     #nodo_nav2_system/tb3
         #cuidado con cuando reconoce que ha terminado
     initial_pose_launch = ExecuteProcess(
@@ -48,97 +42,86 @@ def generate_launch_description():
     #--------- implementar el setup todo lo que tienen los otros xd -------------
     
     return LaunchDescription([
-        # --- nav_to_pose ---
-        launch_nav_to_pose,
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            parameters=[{'use_sim_time': True}, {'yaml_filename': map_file}]
+        ),
+        Node(
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            output='screen',
+            parameters=[nav2_yaml]
+        ),
+        Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_server',
+            output='screen',
+            parameters=[nav2_yaml]
+        ),
+        Node(
+            package='nav2_controller',
+            executable='controller_server',
+            name='controller_server',
+            output='screen',
+            parameters=[nav2_yaml, {'use_sim_time': True}]
+        ),
+        Node(
+            package='nav2_bt_navigator',
+            executable='bt_navigator',
+            name='bt_navigator',
+            output='screen',
+            parameters=[nav2_yaml, {'use_sim_time': True}]
+        ),
+        Node(
+            package='nav2_recoveries',
+            executable='recoveries_server',
+            name='recoveries_server',
+            output='screen',
+            parameters=[nav2_yaml, {'use_sim_time': True}]
+        ),
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_pathplanner',
+            output='screen',
+            parameters=[{'use_sim_time': True},
+                        {'autostart': True},
+                        {'node_names': [
+                            'map_server',
+                            'amcl',
+                            'planner_server',
+                            'controller_server',
+                            'recoveries_server',
+                            'bt_navigator',
+                            'waypoint_follower'
+                        ]}]
+        ),
 
-        # --- nav_to_pose ---
-        RegisterEventHandler(
-            OnProcessStart(
-                target_action=launch_nav_to_pose,
-                on_start=[
-                    Node(
-                        package='nav2_map_server',
-                        executable='map_server',
-                        name='map_server',
-                        output='screen',
-                        parameters=[{'use_sim_time': True}, {'yaml_filename': map_file}]
-                    ),
-                    Node(
-                        package='nav2_amcl',
-                        executable='amcl',
-                        name='amcl',
-                        output='screen',
-                        parameters=[nav2_yaml]
-                    ),
-                    Node(
-                        package='nav2_planner',
-                        executable='planner_server',
-                        name='planner_server',
-                        output='screen',
-                        parameters=[nav2_yaml]
-                    ),
-                    Node(
-                        package='nav2_controller',
-                        executable='controller_server',
-                        name='controller_server',
-                        output='screen',
-                        parameters=[nav2_yaml, {'use_sim_time': True}]
-                    ),
-                    Node(
-                        package='nav2_bt_navigator',
-                        executable='bt_navigator',
-                        name='bt_navigator',
-                        output='screen',
-                        parameters=[nav2_yaml, {'use_sim_time': True}]
-                    ),
-                    Node(
-                        package='nav2_recoveries',
-                        executable='recoveries_server',
-                        name='recoveries_server',
-                        output='screen',
-                        parameters=[nav2_yaml, {'use_sim_time': True}]
-                    ),
-                    Node(
-                        package='nav2_lifecycle_manager',
-                        executable='lifecycle_manager',
-                        name='lifecycle_manager_pathplanner',
-                        output='screen',
-                        parameters=[{'use_sim_time': True},
-                                    {'autostart': True},
-                                    {'node_names': [
-                                        'map_server',
-                                        'amcl',
-                                        'planner_server',
-                                        'controller_server',
-                                        'recoveries_server',
-                                        'bt_navigator',
-                                        'waypoint_follower'
-                                    ]}]
-                    ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': True}],
+            output='screen'
+        ),
 
-                    Node(
-                        package='rviz2',
-                        executable='rviz2',
-                        name='rviz2',
-                        arguments=['-d', rviz_config_dir],
-                        parameters=[{'use_sim_time': True}],
-                        output='screen'
-                    ),
+        Node(
+            package='nav2_waypoint_follower',
+            executable='waypoint_follower',
+            name='waypoint_follower',
+            output='screen',
+            parameters=[nav2_yaml, {'use_sim_time': True}]
+        ),
 
-                    Node(
-                        package='nav2_waypoint_follower',
-                        executable='waypoint_follower',
-                        name='waypoint_follower',
-                        output='screen',
-                        parameters=[nav2_yaml, {'use_sim_time': True}]
-                    ),
-
-                    TimerAction(
-                        period=10.0,
-                        actions=[load_map_launch]
-                    ),
-                ]
-            )
+        TimerAction(
+            period=10.0,
+            actions=[load_map_launch]
         ),
 
         RegisterEventHandler(
